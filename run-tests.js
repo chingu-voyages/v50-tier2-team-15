09bd3af62fc0d8f5546cffa52dcfac6707012047
RossaMania <raclettenberg@gmail.com>
2024-07-16 16:22:04 -0500

    chore: Convert run-tests.sh to run-tests.js for consistency and maintainability. Fix script comments.

--- run-tests.js
+++ run-tests.js
@@ -1,11 +1,14 @@
-# Script to run tests in a Node.js project using Jest.
-# If no tests are defined, the script will exit with a message.
-# This is so that the CI/CD pipeline does not fail when no tests are defined.
+// Script to run tests in a Node.js project using Jest.
+// If no tests are defined, the script will exit with a message.
+// This is so that the CI/CD pipeline does not fail when no tests are defined.
 
-const { execSync } = require('child_process');
-const { existsSync, readdirSync } = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import { readdirSync } from 'fs';
+import path from 'path';
 
+import process from 'process';
+
+const __dirname = path.resolve();
 const testDir = path.join(__dirname, 'src');
 const testFiles = readdirSync(testDir).filter(file => file.endsWith('.test.js'));
 
