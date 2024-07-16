e2e5e3fec46a8c664a3289fc30b5ed201de324da
RossaMania <raclettenberg@gmail.com>
2024-07-15 20:44:29 -0500

    Paste in URL from docs. Set params to desserts for testing.

--- src/foodMenuFetch.js
+++ src/foodMenuFetch.js
@@ -1,7 +1,7 @@
 const foodMenuFetch = async () => {
   try {
     const response = await fetch(
-      "https://tiny-blue-vulture-shoe.cyclic.app/our-foods"
+      "https://tiny-blue-vulture-shoe.cyclic.app/desserts"
     );
     const data = await response.json();
     return data;
