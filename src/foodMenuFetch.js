aca0b7d985cced03ab47e83aa6f3d497f2d239e2
RossaMania <raclettenberg@gmail.com>
2024-07-16 14:10:14 -0500

    fix: Update foodMenuFetch to successfully return fetched data or else return an empty array.

--- src/foodMenuFetch.js
+++ src/foodMenuFetch.js
@@ -1,12 +1,13 @@
 const foodMenuFetch = async () => {
   try {
     const response = await fetch(
-      "https://tiny-blue-vulture-shoe.cyclic.app/desserts"
+      "https://tiny-blue-vulture-shoe.cyclic.app/our-foods"
     );
     const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
+    return [];
   }
 };
 
