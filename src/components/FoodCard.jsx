1f86531558492277ef2fe9f56ea811faf8ea7b37
RossaMania <raclettenberg@gmail.com>
2024-07-17 22:05:46 -0500

    hotfix: comment out FoodCard component code for CI linter.

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,16 +1,16 @@
-const FoodCard = ({ food }) => {
-  if (!food) return <div>Select a food item to see details</div>;
+// const FoodCard = ({ food }) => {
+//   if (!food) return <div>Select a food item to see details</div>;
 
-  return (
-    <div>
-      <h1>{food.name}</h1>
-      <img src={food.img} alt={food.name} />
-      <p>{food.dsc}</p>
-      <p>{food.price}</p>
-      <p>{food.rate}</p>
-      <p>{food.country}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>{food.name}</h1>
+//       <img src={food.img} alt={food.name} />
+//       <p>{food.dsc}</p>
+//       <p>{food.price}</p>
+//       <p>{food.rate}</p>
+//       <p>{food.country}</p>
+//     </div>
+//   );
+// };
 
-export default FoodCard;
+// export default FoodCard;
