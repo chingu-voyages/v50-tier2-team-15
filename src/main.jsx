566fe090ae3b5d7ff2c13d63aaa5b96dcda954e1
RossaMania <raclettenberg@gmail.com>
2024-07-20 14:39:56 -0500

    feat: Add login screen route to react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -14,11 +14,13 @@ import App from "./App.jsx";
 import "./index.css";
 
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
