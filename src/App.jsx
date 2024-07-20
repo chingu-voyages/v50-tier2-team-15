82c01a9454f913794df74c09b1bb8673c056f34b
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-07-20 06:21:11 -0500

    had some problems with merge - a comma error in JSON.package and needed to import iconify.

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,6 @@ function App() {
       <FoodCard />
       
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
