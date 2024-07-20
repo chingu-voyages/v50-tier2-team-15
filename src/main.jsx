1c630ec2ef1e849cbdbb62912049cd7bef21c8dc
RossaMania <raclettenberg@gmail.com>
2024-07-20 14:16:24 -0500

    feat: Create a Browser Route. Add home screen route to react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App.jsx";
 import "./index.css";
 
+import HomeScreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
