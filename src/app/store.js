96b82510991029cfdc798fa7a6211926a97039d0
RossaMania <raclettenberg@gmail.com>
2024-07-20 19:18:43 -0500

    feat: Create authSlice reducer to store user auth info.

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import foodDataReducer from "../slices/foodDataApiSlice";
+import authSliceReducer from "../slices/authSlice";
+
 
 const store = configureStore({
   reducer: {
     foodData: foodDataReducer,
+    auth: authSliceReducer,
   },
 });
 
