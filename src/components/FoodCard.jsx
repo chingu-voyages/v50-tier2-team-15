2a11a6a771ffc41f74ee2fb4a6361717e3d085e7
RossaMania <raclettenberg@gmail.com>
2024-07-27 16:00:50 -0500

    feat: Update the FoodCard component to include an "Add to Cart" button.

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../slices/cartSlice"; // Import addToCart action
 
 const FoodCard = () => {
+  const dispatch = useDispatch();
   const { id } = useParams();
   const foodData = useSelector((state) => state.foodData.data);
   const food = Object.values(foodData).flat().find((item) => item.id === id);
 
   if (!food) return <div>Select a food item to see details</div>;
+
+  const handleAddToCart = () => {
+    dispatch(addToCart({ ...food, qty: 1 })); // Add food item to cart with initial quantity of 1
+  };
 
   return (
     <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
@@ -20,6 +26,7 @@ const FoodCard = () => {
           <p>{food.price}</p>
           <p>{food.rate}</p>
           <p>{food.country}</p>
+          <button onClick={handleAddToCart}>Add to Cart</button> {/* Add to Cart Button */}
         </div>
       </div>
     </div>
