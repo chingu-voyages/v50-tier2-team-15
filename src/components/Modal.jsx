3556b2023d382bf057dae7a24676556b55ba2abb
RossaMania <raclettenberg@gmail.com>
2024-08-02 15:43:59 -0500

    chore: Update Modal component to include LoginScreen and close button.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types';
+import LoginScreen from '../screens/LoginScreen';
 
-const Modal = (props) => {
+const Modal = ({ toggler }) => {
   return (
     <div className="static">
       <div className="fixed h-screen w-screen bg-black z-10 top-0 opacity-75"></div>
       <div className="fixed top-0 right-0 left-0 z-20 flex justify-center">
-        <div className="mx-4 my-4 bg-white">
+        <div className="mx-4 my-4 bg-white p-4 rounded-lg shadow-lg">
           <div className="flex justify-end">
             <button
-              onClick={() => props.toggler()}
+              onClick={() => toggler()}
               className="border-2 text-red-900 px-2 m-2"
             >
               X
             </button>
           </div>
+          <LoginScreen />
         </div>
       </div>
     </div>
