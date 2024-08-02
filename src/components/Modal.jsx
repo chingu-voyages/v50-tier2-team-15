797aae5ee7f577c1b24755b532d881c3e560da49
RossaMania <raclettenberg@gmail.com>
2024-08-02 15:32:17 -0500

    feat: Update Modal component to include close button and props validation.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,27 @@
-const Modal = () => {
+import PropTypes from 'prop-types';
+
+const Modal = (props) => {
   return (
     <div className="static">
       <div className="fixed h-screen w-screen bg-black z-10 top-0 opacity-75"></div>
+      <div className="fixed top-0 right-0 left-0 z-20 flex justify-center">
+        <div className="mx-4 my-4 bg-white">
+          <div className="flex justify-end">
+            <button
+              onClick={() => props.toggler()}
+              className="border-2 text-red-900 px-2 m-2"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Modal
+Modal.propTypes = {
+  toggler: PropTypes.func.isRequired,
+};
+
+export default Modal;
