4525b98b9bc82037d3a722a340be9c0f92f03502
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-08-04 07:57:11 -0500

    moved AddToken buttons to a components and put them in the user dashboard

--- src/screens/UserDashboard.jsx
+++ src/screens/UserDashboard.jsx
@@ -6,27 +6,27 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Cart from "../components/Dashboard/Cart";
 import UserTop from "../components/Dashboard/UserTop";
-import { increaseCurrency } from "../slices/cartSlice";
+// import { increaseCurrency } from "../slices/cartSlice";
 
 const UserDashboard = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const handleCheckout = () => {
     // Navigate to the checkout screen
     navigate("/checkout");
   };
-  const handleAddCurrency = (amount) => {
-    dispatch(increaseCurrency(amount));
-  };
+  // const handleAddCurrency = (amount) => {
+  //   dispatch(increaseCurrency(amount));
+  // };
   return (
     <div>
       <h1>UserDashboard</h1>
       <UserTop />
-      <button onClick={() => handleAddCurrency(50)}>Add 50</button>
+      {/* <button onClick={() => handleAddCurrency(50)}>Add 50</button>
       <button onClick={() => handleAddCurrency(100)}>Add 100</button>
-      <button onClick={() => handleAddCurrency(150)}>Add 150</button>
+      <button onClick={() => handleAddCurrency(150)}>Add 150</button> */}
       <Cart />
       {cartItems.length > 0 ? (
         <button onClick={handleCheckout}>Checkout</button>
