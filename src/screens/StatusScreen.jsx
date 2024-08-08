3e948dff26a368d6adfec8ee699fcbbed77380a2
RossaMania <raclettenberg@gmail.com>
2024-08-08 13:08:47 -0500

    feat: Refactor StatusScreen so order success or failure messages are func components.

--- src/screens/StatusScreen.jsx
+++ src/screens/StatusScreen.jsx
@@ -2,10 +2,36 @@ import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import OrderSummary from "../components/Orders/OrderSummary";
 
+import PropTypes from "prop-types";
+
+const OrderSuccessMessage = ({ lastOrder }) => (
+  <div>
+    <h1>Order Successful!</h1>
+    <p>Your order has been placed successfully!</p>
+    {lastOrder ? (
+      <OrderSummary
+        itemsPrice={lastOrder.itemsPrice}
+        shippingPrice={lastOrder.shippingPrice}
+        taxPrice={lastOrder.taxPrice}
+        totalPrice={lastOrder.totalPrice}
+        currency={lastOrder.currency}
+      />
+    ) : (
+      <p>Loading order details...</p>
+    )}
+  </div>
+);
+
+const OrderFailedMessage = () => (
+  <div>
+    <h1>Order Failed!</h1>
+    <p>Oops! Insufficient tokens to complete purchase!</p>
+  </div>
+);
+
 const StatusScreen = () => {
   const location = useLocation();
   const { orderSuccess, order } = location.state || {};
-
   const lastOrder = useSelector((state) => state.orders?.order) || order;
 
   console.log('Order Success:', orderSuccess);
@@ -15,29 +41,22 @@ const StatusScreen = () => {
   return (
     <div>
       {orderSuccess ? (
-        <div>
-          <h1>Order Successful!</h1>
-          <p>Your order has been placed successfully!</p>
-          {lastOrder ? (
-            <OrderSummary
-              itemsPrice={lastOrder.itemsPrice}
-              shippingPrice={lastOrder.shippingPrice}
-              taxPrice={lastOrder.taxPrice}
-              totalPrice={lastOrder.totalPrice}
-              currency={lastOrder.currency}
-            />
-          ) : (
-            <p>Loading order details...</p>
-          )}
-        </div>
+        <OrderSuccessMessage lastOrder={lastOrder} />
       ) : (
-        <div>
-          <h1>Order Failed!</h1>
-          <p>Oops! Insufficient tokens to complete purchase!</p>
-        </div>
+        <OrderFailedMessage />
       )}
     </div>
   );
+};
+
+OrderSuccessMessage.propTypes = {
+  lastOrder: PropTypes.shape({
+    itemsPrice: PropTypes.number.isRequired,
+    shippingPrice: PropTypes.number.isRequired,
+    taxPrice: PropTypes.number.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default StatusScreen;
