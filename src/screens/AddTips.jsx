d1da5f5dfe95b1e38e248d3507c048db91e8ac10
Nandhini <nandy2991@gmail.com>
2024-08-10 11:21:25 -0500

    styling addtips screen

--- src/screens/AddTips.jsx
+++ src/screens/AddTips.jsx
@@ -16,18 +16,53 @@ const AddTips = () => {
  }
 
  return(
-    <div>
-        <p className="m-4 text-center font-extrabold text-xl">Show us your support by adding Tips</p>
-        <div className="flex space-x-4 justify-center">
-        <button onClick={()=> getTips(0)}>No Tip</button>   
-        <button onClick={()=> getTips(18)}>18%</button>
-        <button onClick={()=> getTips(25)}>25%</button>
-        <button onClick={()=> getTips(35)}>35%</button>
-        <button onClick={()=> getTips(50)}>50%</button>
-        </div>
-        
-        <button className="m-5" onClick={goToOrderScreen}>Next</button>
-    </div>
+ 
+  <div className="bg-white p-6 rounded-lg shadow-md max-w-lg mx-auto">
+  <p className="m-4 text-center font-extrabold text-2xl text-gray-800">
+    Show us your support by adding Tips
+  </p>
+  <div className="flex space-x-4 justify-center mt-6">
+    <button 
+      className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded transition duration-300" 
+      onClick={() => getTips(0)}
+    >
+      No Tip
+    </button>
+    <button 
+      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition duration-300" 
+      onClick={() => getTips(18)}
+    >
+      18%
+    </button>
+    <button 
+      className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded transition duration-300" 
+      onClick={() => getTips(25)}
+    >
+      25%
+    </button>
+    <button 
+      className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded transition duration-300" 
+      onClick={() => getTips(35)}
+    >
+      35%
+    </button>
+    <button 
+      className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded transition duration-300" 
+      onClick={() => getTips(50)}
+    >
+      50%
+    </button>
+  </div>
+
+  <div className="flex justify-center">
+    <button 
+      className="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-6 rounded mt-8 transition duration-300"
+      onClick={goToOrderScreen}
+    >
+      Next
+    </button>
+  </div>
+</div>
  )
 }
 export default AddTips
