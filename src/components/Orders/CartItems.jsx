24cd929fd72e5c0d151ce8a410b815ccead84df7
RossaMania <raclettenberg@gmail.com>
2024-08-11 16:48:08 -0500

    fix: Add star icons to CartItems components. Delete whitespace in Results and CartItems. Delete $ in Results. Round up all numbers to 2 decimals.

--- src/components/Orders/CartItems.jsx
+++ src/components/Orders/CartItems.jsx
@@ -1,6 +1,23 @@
 import PropTypes from "prop-types";
+import { Icon } from '@iconify/react';
+import starFilled from '@iconify/icons-mdi/star'; // Import star icon
 
 const CartItems = ({ items }) => {
+  const renderStars = (rate) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      stars.push(
+        <Icon
+          key={i}
+          icon={starFilled}
+          className={i < rate ? 'text-yellow-400' : 'text-gray-300'}
+        />
+      );
+    }
+    return stars;
+  };
+
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md">
       <h2 className="text-xl font-semibold mb-4">Cart Items</h2>
@@ -11,7 +28,7 @@ const CartItems = ({ items }) => {
               <div className="flex-1">
                 <div className="text-lg font-semibold">{item.dsc}</div>
                 <div className="text-sm text-gray-600">From: {item.name}</div>
-                <div className="text-sm text-gray-600">Rating: {item.rate}</div>
+                <div className="flex mb-4 m-auto p-auto justify-center align-center">{renderStars(item.rate)}</div>
               </div>
               <div className="mt-2 sm:mt-0 text-lg font-semibold text-right">
                 x {item.qty}
