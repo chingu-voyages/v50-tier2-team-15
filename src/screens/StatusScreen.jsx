defd4ca8d0bcec79062c26478d0a0332f55473f4
RossaMania <raclettenberg@gmail.com>
2024-08-10 22:14:55 -0500

    chore: Update StatusScreen to display order information in a grid.

--- src/screens/StatusScreen.jsx
+++ src/screens/StatusScreen.jsx
@@ -8,15 +8,19 @@ const StatusScreen = () => {
   const lastOrder = useSelector((state) => state.orders?.order) || order;
   const currentTokens = useSelector((state) => state.auth.userInfo.tokens); // Use tokens from authSlice
 
-  console.log('Order Success:', orderSuccess);
-  console.log('Order from location state:', order);
-  console.log('Order from Redux state:', lastOrder);
-  console.log('Current Tokens:', currentTokens);
+  console.log("Order Success:", orderSuccess);
+  console.log("Order from location state:", order);
+  console.log("Order from Redux state:", lastOrder);
+  console.log("Current Tokens:", currentTokens);
 
   return (
     <div>
       {orderSuccess ? (
-        <OrderSuccessMessage lastOrder={lastOrder} currentTokens={currentTokens} savedAddress={savedAddress}/>
+        <OrderSuccessMessage
+          lastOrder={lastOrder}
+          currentTokens={currentTokens}
+          savedAddress={savedAddress}
+        />
       ) : (
         <OrderFailedMessage />
       )}
@@ -28,45 +32,56 @@ const OrderSuccessMessage = ({ lastOrder, currentTokens, savedAddress }) => (
   <div className="p-4">
     <h1 className="text-3xl font-semibold mb-4">Order Successful!</h1>
     <p className="text-lg mb-4">Your order has been placed successfully!</p>
-    {savedAddress && (
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold mb-2">Shipping Address</h2>
-        <p><strong>Street Address:</strong> {savedAddress.address}</p>
-        <p><strong>City:</strong> {savedAddress.city}</p>
-        <p><strong>Postal Code:</strong> {savedAddress.postalCode}</p>
-      </div>
-    )}
-    {lastOrder ? (
-      <div className="space-y-2">
-        <p className="text-lg">
-          <strong>Items Price:</strong> {lastOrder.itemsPrice}
-        </p>
-        <p className="text-lg">
-          <strong>Shipping Price:</strong> {lastOrder.shippingPrice}
-        </p>
-        <p className="text-lg">
-          <strong>Tax Price:</strong> {lastOrder.taxPrice}
-        </p>
-        <p className="text-lg">
-          <strong>Tip:</strong> {lastOrder.tipsTotal.toFixed(2)}
-        </p>
-        <p className="text-lg">
-          <strong>Order Total:</strong> {lastOrder.totalPrice.toFixed(2)}
-        </p>
-        <p className="text-lg">
-          <strong>Current Tokens:</strong> {currentTokens}
-        </p>
-      </div>
-    ) : (
-      <p>Loading order details...</p>
-    )}
+    <div className="grid grid-cols-2">
+      {savedAddress && (
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold mb-2">Shipping Address</h2>
+          <p className="text-lg">
+            <strong>Street Address:</strong> {savedAddress.address}
+          </p>
+          <p className="text-lg">
+            <strong>City:</strong> {savedAddress.city}
+          </p>
+          <p className="text-lg">
+            <strong>Postal Code:</strong> {savedAddress.postalCode}
+          </p>
+        </div>
+      )}
+      {lastOrder ? (
+        <div className="space-y-2">
+          <h2 className="text-xl font-semibold mb-2">Order Details</h2>
+          <p className="text-lg">
+            <strong>Items Price:</strong> {lastOrder.itemsPrice}
+          </p>
+          <p className="text-lg">
+            <strong>Shipping Price:</strong> {lastOrder.shippingPrice}
+          </p>
+          <p className="text-lg">
+            <strong>Tax Price:</strong> {lastOrder.taxPrice}
+          </p>
+          <p className="text-lg">
+            <strong>Tip:</strong> {lastOrder.tipsTotal.toFixed(2)}
+          </p>
+          <p className="text-lg">
+            <strong>Order Total:</strong> {lastOrder.totalPrice.toFixed(2)}
+          </p>
+          <p className="text-lg">
+            <strong>Current Tokens:</strong> {currentTokens}
+          </p>
+        </div>
+      ) : (
+        <p>Loading order details...</p>
+      )}
+    </div>
   </div>
 );
 
 const OrderFailedMessage = () => (
   <div className="bg-gray-800 text-white max-w-md mx-auto p-8 rounded-lg">
     <h1 className="text-3xl font-bold mb-4 text-red-600">Order Failed!</h1>
-    <p className="text-lg text-gray-300">Oops! Insufficient tokens to complete purchase!</p>
+    <p className="text-lg text-gray-300">
+      Oops! Insufficient tokens to complete purchase!
+    </p>
   </div>
 );
 
