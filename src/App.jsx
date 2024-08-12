bbcb8e1a6499f22e5018f1c8e9ade732f92616ff
RossaMania <raclettenberg@gmail.com>
2024-08-11 20:24:41 -0500

    refactor: Remove unused CSS styles and update scroll-to-top functionality in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LoginModal from "./components/LoginModal";
 import FoodCardModal from "./components/FoodCardModal";
 import useToggle from "./utils/useToggle";
+import { useState, useEffect } from "react";
+import { FaRegArrowAltCircleUp } from "react-icons/fa";
 
 function App() {
-
   const { on, toggler } = useToggle(); // Destructure the on and toggler properties from the useToggle hook function.
 
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) { // Adjust this value based on when you want the button to appear
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <>
       <Navbar toggler={toggler} />
       <main className="mx-auto">
         <Outlet />
+        {showBackToTop && (
+          <button
+            className="fixed bottom-4 right-4 text-lightOrange bg-transparent rounded-full text-5xl drop-shadow-none hover:drop-shadow-none focus:bg-transparent"
+            onClick={scrollToTop}
+          >
+            <FaRegArrowAltCircleUp />
+          </button>
+        )}
       </main>
       <Footer />
       {on && <LoginModal toggler={toggler} />}
@@ -25,24 +53,3 @@ function App() {
 }
 
 export default App;
-
-// import "./App.css";
-// import { Outlet } from "react-router-dom";
-
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <FoodRibbon />
-//       <main className="mx-auto py-5">
-//         <Outlet />
-//       </main>
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
