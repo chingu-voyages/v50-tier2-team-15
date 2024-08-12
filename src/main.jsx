cafac7862d042d262f55861ef06ea5c53667af9e
RossaMania <raclettenberg@gmail.com>
2024-08-11 19:23:56 -0500

    refactor: Remove LoginScreen file, update routing in main.jsx, and remove LoginScreen imports.

--- src/main.jsx
+++ src/main.jsx
@@ -15,10 +15,8 @@ import App from "./App.jsx";
 import "./index.css";
 
 import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
 import UserDashboard from "./screens/UserDashboard";
 import ShowFoodMenu from "./screens/ShowFoodMenu.jsx";
-// import FoodCard from "./components/FoodCard";
 import OurFoodsList from "./screens/OurFoodsList.jsx";
 import OrderScreen from "./screens/OrderScreen.jsx";
 import StatusScreen from "./screens/StatusScreen.jsx";
@@ -27,7 +25,6 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/login" element={<LoginScreen />} />
       <Route path="/foods" element={<OurFoodsList />} />
       <Route path="/foods/:category" element={<ShowFoodMenu />} />
       {/* <Route path="/food/:id" element={<FoodCard />} /> */}
