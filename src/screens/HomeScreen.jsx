4426e1469d87aa173860e8a80b833973bcabc2a0
RossaMania <raclettenberg@gmail.com>
2024-08-11 20:26:09 -0500

    refactor: Remove unused useState and useEffect imports in HomeScreen.jsx

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchFoodData } from "../slices/foodDataApiSlice";
 import { useNavigate } from "react-router";
@@ -8,15 +8,11 @@ import HomeSpecialties from "../components/HomeScreen/HomeSpecialties";
 import HomeCategories from "../components/HomeScreen/HomeCategories";
 import HomeLocation from "../components/HomeScreen/HomeLocation";
 import HomeBottom from "../components/HomeScreen/HomeBottom";
-import { FaRegArrowAltCircleUp } from "react-icons/fa";
-
-
 
 const HomeScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const [showBackToTop, setShowBackToTop] = useState(false);
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
@@ -26,27 +22,7 @@ const HomeScreen = () => {
 
   useEffect(() => {
     dispatch(fetchFoodData());
-
-    const handleScroll = () => {
-      const ribbon = document.querySelector("#foodRibbon"); // Add an id to FoodRibbon's wrapper div
-      const ribbonTop = ribbon?.offsetTop || 0;
-
-      if (window.scrollY > ribbonTop) {
-        setShowBackToTop(true);
-      } else {
-        setShowBackToTop(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, [dispatch]);
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
 
   return (
     <div className="bg-gradient-to-b from-white via-lightOrange to-orange via-lightOrange to-white">
@@ -57,14 +33,6 @@ const HomeScreen = () => {
         <HomeCategories />
         <HomeLocation />
         <HomeBottom />
-        {showBackToTop && (
-          <button
-            className="fixed bottom-3 -right-3 text-lightOrange bg-transparent rounded-full hidden md:block text-5xl drop-shadow-none hover:drop-shadow-none focus:bg-transparent"
-            onClick={scrollToTop}
-          >
-            <FaRegArrowAltCircleUp />
-          </button>
-        )}
       </div>
     </div>
   );
